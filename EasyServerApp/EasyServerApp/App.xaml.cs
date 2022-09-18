3413ee78365139eaf1f424aa94e6d8377c008eb7
jripperger <rippergerjacob@gmail.com>
2022-09-18 15:01:59 -0400

    Initial Commit

---
+++ EasyServerApp/EasyServerApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyServerApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ EasyServerApp/EasyServerApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyServerApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
