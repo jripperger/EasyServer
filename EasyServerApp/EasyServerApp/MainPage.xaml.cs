e0340ea7274275cfa46d7507fa02d07fe293a01a
jripperger <rippergerjacob@gmail.com>
2022-12-05 19:11:57 -0500

    Documentation Complete

--- EasyServerApp/EasyServerApp/MainPage.xaml.cs
+++ EasyServerApp/EasyServerApp/MainPage.xaml.cs
@@ -6,20 +6,23 @@ namespace EasyServerApp;
 
 public partial class MainPage : ContentPage
 {
-    private EasyServerRepository _easyServerRepository;
-    public EasyServerRepository easyServerRepository { get { return _easyServerRepository; }  }
+    private EasyServerRepository _easyServerRepository;  // "API"
+    private Hashtable requestServiceStates;              // States for whether or not customers are awaiting service at each table       
 
-    private Hashtable requestServiceStates;
+    public EasyServerRepository easyServerRepository { get { return _easyServerRepository; }  }
 
     public MainPage()
 	{
 		InitializeComponent();
+
+        // Create a singleton of the pseudo API
         CreateAPI();
 
-        requestServiceStates= new Hashtable();
+        requestServiceStates = new Hashtable();
 
         List<RestaurantTable> tables = easyServerRepository.RestaurantTables;
 
+        // Add a service request state entry for each table to the hashtable
         for (int i = 0; i < tables.Count; i++)
         {
             requestServiceStates[tables[i].TableId] = false;
--- EasyServerApp/EasyServerApp/ServerQueue.cs
+++ EasyServerApp/EasyServerApp/ServerQueue.cs
@@ -1,22 +1,27 @@
-﻿using EasyServerApp.EasyServerDB;
-using Microsoft.Maui.Handlers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * ServerQueue
+ * 
+ * This class represents a server queue. Each server's queue has an associated
+ * employee and a queue, which is implemented as a list of RestaurantTable 
+ * objects.
+ * 
+ */
+
+using EasyServerApp.EasyServerDB;
 
 namespace EasyServerApp
 {
     public class ServerQueue
     {
-        private List<RestaurantTable> serverQueue;
-        public List<RestaurantTable> Queue { get { return serverQueue; } set { serverQueue = value; } }
+        private List<RestaurantTable> serverQueue;  // List of tables awaiting service from the associated server
+        private Employee employee;                  // Server associated with the queue
 
-        private Employee employee;
+        public List<RestaurantTable> Queue { get { return serverQueue; } set { serverQueue = value; } }       
         public Employee Employee { get { return employee; } private set { employee = value; } }
 
         public ServerQueue(Employee employee, List<RestaurantTable> serverQueue) {
+
+            // Assign values to global variables
             this.serverQueue = serverQueue;
             this.employee = employee;
         }
